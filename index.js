a7281539d27a5d030cbcb2ec9620f4e1c8694778
Felipe Passos <felipekaires@hotmail.com>
2017-10-22 23:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  var c =  document.getElementById('grand-node').lastChild.innerHTML;
+  var c =  document.querySelectorAll('grande-node')
   var a =  document.getElementById('grand-node').querySelectorAll(`${c}`);
 
   for (var i = 0; i < a.length; i++) {
