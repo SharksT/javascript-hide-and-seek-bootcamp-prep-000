7a3bc4fd140aaf27ee60279be59ef48f533299b7
Felipe Passos <felipekaires@hotmail.com>
2017-10-22 23:46:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,5 +25,7 @@ function deepestChild() {
 function increaseRankBy(n) {
 
 var a =  document.getElementById('app').querySelectorAll('ul.ranked-list li')
-
+for (let i = 0; i < a.length; i++) {
+  a[i].innerHTML = (a[i].innerHTML + n).toString()
+}
 }
