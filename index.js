8781b6dc4aa5557f67acc42c49fd01366e409014
Felipe Passos <felipekaires@hotmail.com>
2017-10-22 23:56:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+function getFirstSelector(selector) {
+
+  return document.querySelector(selector);
+
+}
+
+function nestedTarget() {
+
+  var a =  document.getElementById('nested').querySelectorAll('div.target');
+
+  for (var i = 0; i < a.length; i++) {
+    return a[i];
+  }
+}
+
+function deepestChild() {
+  var c =  document.getElementById('grand-node').querySelectorAll('*');
+  var last = c[c.length -1 ]
+  return last;
+
+}
+
+function increaseRankBy(n) {
+
+var a =  document.getElementById('app').querySelectorAll('ul.ranked-list li')
+for (let i = 0; i < a.length; i++) {
+  a[i].innerHTML = (parseInt(a[i].innerHTML) + n).toString()
+}
+}
