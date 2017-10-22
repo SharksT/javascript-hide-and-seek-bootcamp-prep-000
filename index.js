50c2cf790cfbd47ee43f534107ef85c4b5c25a2d
Felipe Passos <felipekaires@hotmail.com>
2017-10-22 22:58:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nestedTarget() {
   var a =  document.getElementById('nested').querySelector('div.nested');
 
   for (var i = 0; i < a.length; i++) {
-    a[i]
+    return a[i].innerHTML
   }
 
 }
